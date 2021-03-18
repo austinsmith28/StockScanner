da5be4555b16556af28b0b3af170fecfc4d72a60
austinsmith28 <32520537+austinsmith28@users.noreply.github.com>
2021-03-18 15:38:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-#comment for commit and push
+
 
 
 # Press the green button in the gutter to run the script.
