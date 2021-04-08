516db00d6f7bbaab6300cad1ac2f38a44bdae4ca
BuildTools <unconfigured@null.spigotmc.org>
2021-04-08 14:28:27 -0400

    finviz scraping example

--- testAPIcalls.py
+++ testAPIcalls.py
@@ -9,7 +9,7 @@ stock_price = requests.get(price_url).json()
 
 print(stock_price['price'])'''
 
-
+'''
 allStocksUrl = f'https://api.twelvedata.com/stocks'
 
 allStockData = requests.get(allStocksUrl).json()
@@ -18,6 +18,8 @@ allSymbols = []
 
 for i in allStockData['data']:
     print(i['symbol'], " is on ", i['exchange'], " in the ", i['country'])
+'''
+
 '''
 url = "https://finance.yahoo.com/quote/"
 
@@ -34,3 +36,18 @@ print(type(price))
 
 #float_stock_price = float(stock_price)
 '''
+
+
+url = "https://finviz.com/quote.ashx?t="
+
+full_url = url + "aapl"
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/68.0.3440.106 Safari/537.36', }
+response = requests.get(full_url, headers=headers).content
+
+soup = bs4.BeautifulSoup(response, 'html.parser')
+
+average_volume = soup.find("td", text="Avg Volume").find_next_sibling("td").text
+
+print(average_volume)
