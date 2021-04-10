d1df7b4589ca472644102c164ac16685e9a18546
jwesgore <jwesgore@gmail.com>
2021-04-10 16:22:57 -0400

    add support for building dictionary with submit button

--- gui/Toolbar.py
+++ gui/Toolbar.py
@@ -25,7 +25,24 @@ class Indicator(Spinner):
 
 class Toolbar(Widget):
     def search(self):
-        pass
+
+        fundamentals = self.ids.fundamentals.ids
+        technical = self.ids.technical.ids
+
+        dict = {
+            "asset": self.ids.asset.text,
+
+            "vol_low": fundamentals.volume_low.text,
+            "vol_high": fundamentals.volume_high.text,
+            "mktcap_low": fundamentals.mktcap_low.text,
+            "mktcap_high": fundamentals.mktcap_high.text,
+            "change_low": fundamentals.change_low.text,
+            "change_high": fundamentals.change_high.text,
+
+            "timeperiod": technical.timeperiod.text,
+            "indicator": technical.indicator.text,
+            "threshold": technical.threshold.text
+        }
 
 
 class Main(App):
