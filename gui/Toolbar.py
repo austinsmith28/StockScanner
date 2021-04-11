c900a935fe2b766e30ed72d33cbe082b10ea4140
jwesgore <jwesgore@gmail.com>
2021-04-11 15:59:20 -0400

    updated to add price, share, and short

--- gui/Toolbar.py
+++ gui/Toolbar.py
@@ -32,10 +32,16 @@ class Toolbar(Widget):
         dict = {
             "asset": self.ids.asset.text,
 
+            "price_low": fundamentals.price_low.text,
+            "price_high": fundamentals.price_high.text,
             "vol_low": fundamentals.volume_low.text,
             "vol_high": fundamentals.volume_high.text,
             "mktcap_low": fundamentals.mktcap_low.text,
             "mktcap_high": fundamentals.mktcap_high.text,
+            "share_low": fundamentals.share_low.text,
+            "share_high": fundamentals.share_high.text,
+            "short_low": fundamentals.short_low.text,
+            "short_high": fundamentals.short_high.text,
             "change_low": fundamentals.change_low.text,
             "change_high": fundamentals.change_high.text,
 
