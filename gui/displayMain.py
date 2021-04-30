5b0302caff0b12f1ff58220681184df47616a410
jwesgore <jwesgore@gmail.com>
2021-04-30 15:49:50 -0400

    add files

--- gui/displayMain.py
+++ gui/displayMain.py
@@ -1,12 +1,15 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 
 Builder.load_file('displayMain.kv')
 
 display = Widget
+tlist ={}
+p = 0
 
 class Display(BoxLayout):
 
@@ -15,15 +18,30 @@ class Display(BoxLayout):
         global display
         display = self
 
-    def build(self, tlist):
+    def build(self, list):
         x = 0
+        display.clear_widgets()
+        global tlist
+        global p
+        tlist = list
+        p = 0
         print("blubber boy")
-        for i in tlist:
-            x += 1
-            display.add_widget(Label(text=i))
-            if x == 25:
-                break
+
+        for i in range(24):
+            display.add_widget(Label(text=tlist[i]))
+        display.add_widget(Button(text="next", on_release=Display.next(self)))
+
         return self
+
+    def next(self):
+        print("beotch")
+        display.clear_widgets()
+        global p
+        p = p + 1
+        for i in range(24):
+            display.add_widget(Label(text=tlist[i + (p * 24)]))
+        display.add_widget(Button())
+
 
 
 class Main(App):
