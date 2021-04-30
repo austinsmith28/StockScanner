84aeb8a81e1d13e3f44a5277eba82806adc547cb
jwesgore <jwesgore@gmail.com>
2021-04-30 17:33:15 -0400

    previous button

--- gui/displayMain.py
+++ gui/displayMain.py
@@ -43,33 +43,33 @@ class Display(BoxLayout):
         for i in range(24):
             display.add_widget(DisplayLabel(text=tlist[i + (p * 24)]))
 
-        display.previous = Button(text="previous")
-        display.previous.bind(on_release=display.next)
-        display.add_widget(display.previous)
+        display.submit = Button(text="previous")
+        display.submit.bind(on_release=display.previous)
+        display.add_widget(display.submit)
 
         display.submit = Button(text="next")
         display.submit.bind(on_release=display.next)
         display.add_widget(display.submit)
 
     def previous(self, instance):
-        print("beotch")
+        print("the last beotch")
         global p
         global cap
 
-        p = p - 1
-        if p < cap:
+        if p > 0:
+            p = p - 1
             display.clear_widgets()
             Display.ihatethis(self, p)
         else:
             print("ur dad")
 
     def next(self, instance):
-        print("beotch")
+        print("the next beotch")
         global p
         global cap
 
-        p = p + 1
         if p < cap:
+            p = p + 1
             display.clear_widgets()
             Display.ihatethis(self, p)
         else:
